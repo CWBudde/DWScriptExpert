f772927fb7415a042fff538dcd2480786e89ba20
CWBudde <Christian@savioursofsoul.de>
2015-06-01 22:09:16 +0200

    Added DLL support (to avoid package conflicts)

---
+++ DLL/DWScriptExpert.dpr
@@ -0,0 +1,13 @@
+library DWScriptExpert;
+
+uses
+  ShareMem,
+  SysUtils,
+  Classes,
+  DWScriptExpertForm in '..\Source\DWScriptExpertForm.pas' {DWScriptExpertDockForm},
+  DWScriptExpertModule in '..\Source\DWScriptExpertModule.pas' {DataModuleScript: TDataModule},
+  DWScriptExpertAbout in '..\Source\DWScriptExpertAbout.pas' {FormAbout};
+
+begin
+end.
+
