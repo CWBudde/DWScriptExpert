0b1a81da19edd98bb9f9b546b25b97f2df50a04f
Christian <Christian@savioursofsoul.de>
2016-08-24 09:21:18 +0200

    Added lib suffix

--- DLL/DWScriptExpert.dpr
+++ DLL/DWScriptExpert.dpr
@@ -1,5 +1,33 @@
 library DWScriptExpert;
 
+{$IFDEF VER210} // RAD Studio 2010
+{$LIBSUFFIX 'D2010'}
+{$ENDIF}
+{$IFDEF VER220} // RAD Studio XE
+{$LIBSUFFIX 'XE'}
+{$ENDIF}
+{$IFDEF VER230} // RAD Studio XE2
+{$LIBSUFFIX 'XE2'}
+{$ENDIF}
+{$IFDEF VER240} // RAD Studio XE3
+{$LIBSUFFIX 'XE3'}
+{$ENDIF}
+{$IFDEF VER250} // RAD Studio XE4
+{$LIBSUFFIX 'XE4'}
+{$ENDIF}
+{$IFDEF VER260} // RAD Studio XE5
+{$LIBSUFFIX 'XE5'}
+{$ENDIF}
+{$IFDEF VER270} // RAD Studio XE6
+{$LIBSUFFIX 'XE6'}
+{$ENDIF}
+{$IFDEF VER280} // RAD Studio XE7
+{$LIBSUFFIX 'XE7'}
+{$ENDIF}
+{$IFDEF VER290} // RAD Studio XE8
+{$LIBSUFFIX 'XE8'}
+{$ENDIF}
+
 uses
   ShareMem,
   SysUtils,
